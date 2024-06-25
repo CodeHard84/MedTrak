32cf8b85a829183a7a13b721ebdf1e19f9d521fb
Jace <sockgnome@att.net>
2024-06-25 15:00:30 -0500

    Initial user auth commit.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    redirectUri={window.location.origin}
+    audience={audience}
+  >
     <App />
-  </React.StrictMode>,
-)
+  </Auth0Provider>
+);

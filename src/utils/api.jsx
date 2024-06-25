d72ae580c86b7205b1049e4d5c0c198c57b2607a
Jace <sockgnome@att.net>
2024-06-25 17:48:01 -0500

    Added testing.

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -6,8 +6,10 @@ export const useApi = () => {
 
   const callApi = async (endpoint) => {
     try {
-      const token = await getAccessTokenSilently();
-      console.log(`Calling API with token: ${token}`);
+      const token = await getAccessTokenSilently({
+        audience: process.env.VITE_AUTH0_AUDIENCE,
+      });
+      console.log("JWT Token:", token); // Log the token to verify it
       const response = await axios.get(endpoint, {
         headers: {
           Authorization: `Bearer ${token}`,

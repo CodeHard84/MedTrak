2967ad1e6130e70913fbd1d5cda85ec6b2d2071d
Jace <sockgnome@att.net>
2024-06-26 00:32:40 -0500

    Auth0 changes for remote support.

--- src/api/medications.js
+++ src/api/medications.js
@@ -2,43 +2,43 @@ import axios from 'axios';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const useApi = () => {
-  const { getAccessTokenSilently } = useAuth0();
+  const { getIdTokenClaims } = useAuth0();
 
   const getMedications = async () => {
-    const token = await getAccessTokenSilently();
+    const token = await getIdTokenClaims();
     const response = await axios.get('https://medtrakback.onrender.com/api/medications', {
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${token.__raw}`,
       },
     });
     return response.data;
   };
 
   const createMedication = async (medication) => {
-    const token = await getAccessTokenSilently();
+    const token = await getIdTokenClaims();
     const response = await axios.post('https://medtrakback.onrender.com/api/medications', medication, {
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${token.__raw}`,
       },
     });
     return response.data;
   };
 
   const updateMedication = async (id, medication) => {
-    const token = await getAccessTokenSilently();
+    const token = await getIdTokenClaims();
     const response = await axios.put(`https://medtrakback.onrender.com/api/medications/${id}`, medication, {
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${token.__raw}`,
       },
     });
     return response.data;
   };
 
   const deleteMedication = async (id) => {
-    const token = await getAccessTokenSilently();
+    const token = await getIdTokenClaims();
     await axios.delete(`https://medtrakback.onrender.com/api/medications/${id}`, {
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${token.__raw}`,
       },
     });
   };

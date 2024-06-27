23a2399a664453255f531929cb4617791b4ff71b
Jace <sockgnome@att.net>
2024-06-27 18:06:10 -0500

    refactor: Improve loading state in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 import MedicationsList from './components/MedicationsList';
 import CreateMedication from './components/CreateMedication';
 import EditMedication from './components/EditMedication';
 import NavBar from './components/NavBar';
-import { Container } from 'react-bootstrap'; // Import Container from react-bootstrap
+import { Container, Spinner } from 'react-bootstrap'; // Added Spinner for loading state
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   const { isLoading, isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <Container className="text-center mt-5">
+        <Spinner animation="border" />
+        <p>Loading...</p>
+      </Container>
+    );
   }
 
   return (
@@ -24,9 +29,10 @@ const App = () => {
       />
       <Container className="mt-3">
         <Routes>
-          <Route path="/" element={<MedicationsList />} />
-          <Route path="/create" element={<CreateMedication />} />
-          <Route path="/edit/:id" element={<EditMedication />} />
+          <Route path="/" element={isAuthenticated ? <MedicationsList /> : <Navigate to="/login" />} />
+          <Route path="/create" element={isAuthenticated ? <CreateMedication /> : <Navigate to="/login" />} />
+          <Route path="/edit/:id" element={isAuthenticated ? <EditMedication /> : <Navigate to="/login" />} />
+          <Route path="/login" element={<div className="text-center mt-5"><button onClick={() => loginWithRedirect()}>Log In</button></div>} />
         </Routes>
       </Container>
     </div>

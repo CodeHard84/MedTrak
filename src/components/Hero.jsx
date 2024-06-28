83f0c6c4fcccb9e92dfedca21f02d3cd68b6b0a7
Jace <sockgnome@att.net>
2024-06-27 20:09:33 -0500

    Refactor Hero component to use a Container instead of Jumbotron because the tutorial was old and deprecated.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import { Container, Jumbotron, Button } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const Hero = () => {
   const { loginWithRedirect } = useAuth0();
 
   return (
-    <Jumbotron fluid className="text-center">
-      <Container>
-        <h1>Welcome to MedTrak</h1>
-        <p>Please log in to manage your medications.</p>
-        <Button onClick={() => loginWithRedirect()}>Log In</Button>
-      </Container>
-    </Jumbotron>
+    <Container fluid className="text-center py-5 bg-light">
+      <h1>Welcome to MedTrak</h1>
+      <p>Please log in to manage your medications.</p>
+      <Button onClick={() => loginWithRedirect()}>Log In</Button>
+    </Container>
   );
 };
 

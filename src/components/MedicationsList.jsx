9db54546d2405b1c178289e3e0ad7a2228f9812c
Jace <sockgnome@att.net>
2024-06-27 22:10:13 -0500

    chore: Fix typo in MedicationsList delete confirmation message

--- src/components/MedicationsList.jsx
+++ src/components/MedicationsList.jsx
@@ -81,7 +81,7 @@ const MedicationsList = () => {
         <Modal.Header closeButton>
           <Modal.Title>Confirm Deletion</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Are you sure you want to delete the medication "{deleteMedicationDetails?.name}"?</Modal.Body>
+        <Modal.Body>Are you sure you want to delete the medication {deleteMedicationDetails?.name}?</Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleCloseModal}>
             Cancel
